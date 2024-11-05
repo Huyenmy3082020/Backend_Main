8c22f81604bff1907377ed5fd6e01509b812a718
Huyenmy30082020 <Hatuan1562004@gmail.com>
2024-11-05 23:25:58 +0700

    a

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-require("dotenv").config(); // Thêm dotenv
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // Khai báo mongoose không cần destructuring
 const routes = require("./routes");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 2000;
+const port = 2000;
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -15,7 +14,6 @@ app.use(cookieParser());
 const errorHandler = require("./middleware/errorHandler");
 
 app.use(cors());
-
 app.use(errorHandler);
 routes(app);
 
@@ -23,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
-const MONGO_DB = process.env.MONGO_DB; // Sử dụng biến môi trường
+const MONGO_DB = "DDZleMPJY4GL7Vsp";
 mongoose
   .connect(
     `mongodb+srv://Hahuyenmy:${MONGO_DB}@cluster0.lcgny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
