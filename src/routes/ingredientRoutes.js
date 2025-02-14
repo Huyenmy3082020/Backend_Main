8a7d507d75f5ee093fd4d1eaeebf059d07b30c64
VuVietCuong <cuonghaiduongg2004@gmail.com>
2025-02-14 10:13:35 +0700

    j

--- src/routes/ingredientRoutes.js
+++ src/routes/ingredientRoutes.js
@@ -9,7 +9,7 @@ const {
 router.post("/", ingredientController.createIngredient); // Thêm mới
 router.get("/", ingredientController.getAllIngredients); // Lấy tất cả
 router.get("/:id", ingredientController.getIngredientById); // Lấy theo ID
-router.put("/:id", authMiddleware, ingredientController.updateIngredient); // Cập nhật
+router.put("/:id", ingredientController.updateIngredient); // Cập nhật
 router.delete("/:id", ingredientController.deleteIngredient); // Xóa
 
 module.exports = router;
