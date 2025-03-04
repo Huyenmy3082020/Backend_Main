8f78e29d855f84f29cd32fdc3f21ce0152ba9053
nguyen <nguyen@ubuntu22.myguest.virtualbox.org>
2025-03-04 23:02:44 +0700

    tranh loi truoc khi pull

--- config/elasticsearch/index.js
+++ config/elasticsearch/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Client } = require("@elastic/elasticsearch");
 
-const esClient = new Client({ node: "http://localhost:9200" }); // Đổi thành localhost
+const esClient = new Client({ node: "http://elasticsearch:9200" }); // Đổi thành localhost
 
 module.exports = { esClient };
