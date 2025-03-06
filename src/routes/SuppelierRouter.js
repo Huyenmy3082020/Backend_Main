a942784ca8338d5703084d24b229fb1c98f37e62
Huyenmy30082020 <Hatuan1562004@gmail.com>
2025-03-06 11:35:51 +0700

    final nua nay

--- src/routes/SuppelierRouter.js
+++ src/routes/SuppelierRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const supplierController = require("../controller/SupperlierController");
+const { authenticateToken } = require("../middleware/authmiddleware");
 
-router.get("/", supplierController.getAllSuppliers);
+router.get("/", authenticateToken, supplierController.getAllSuppliers);
 
 module.exports = router;
